8dc720e460eda3bdfdcdebf432f2383889a0dce0
instadotanalytics <96242680+instadotanalytics@users.noreply.github.com>
2026-04-08 11:47:24 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -219,7 +219,7 @@ app.get('/api/test', (req, res) => {
 app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
 // 👇 ye fallback add karo (VERY IMPORTANT)
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
 });
 
